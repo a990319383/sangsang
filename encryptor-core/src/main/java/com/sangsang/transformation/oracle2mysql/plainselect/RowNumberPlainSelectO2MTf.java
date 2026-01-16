f60d2d0e100a76a6add71a069489a147f9bdbddf
刘唐麒 <liutangqi@sinoiov.com>
2026-01-16 14:15:37 +0800

    【DEPLOY】3.6.1-alpha版本发布中央仓库

---
+++ encryptor-core/src/main/java/com/sangsang/transformation/oracle2mysql/plainselect/RowNumberPlainSelectO2MTf.java
@@ -0,0 +1,24 @@
+/*
+package com.sangsang.transformation.oracle2mysql.plainselect;
+
+import com.sangsang.domain.dto.PlainSelectTransformationDto;
+import com.sangsang.transformation.PlainSelectTransformation;
+
+*/
+/**
+ * @author liutangqi
+ * @date 2026/1/15 18:00
+ *//*
+
+public class RowNumberPlainSelectO2MTf extends PlainSelectTransformation {
+    @Override
+    public boolean needTransformation(PlainSelectTransformationDto plainSelectTransformationDto) {
+        return false;
+    }
+
+    @Override
+    public PlainSelectTransformationDto doTransformation(PlainSelectTransformationDto plainSelectTransformationDto) {
+        return null;
+    }
+}
+*/
